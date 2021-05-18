735fc11a7a53417bf4e37a801a57121e9a254356
joaodore <joaodore@gmail.com>
2021-05-17 22:10:02 -0300

    fixing mais.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -21,22 +21,30 @@ resource "aws_instance" "challenge" {
   //  subnet_id              = aws_subnet.challenge-subnet.id
   vpc_security_group_ids      = [aws_security_group.challenge-sg.id]
   associate_public_ip_address = true
-  user_data                   = data.template_file.lauch.template
+  user_data                   = data.template_file.launch.template
   tags = {
     Name = "challenge${count.index}"
   }
 }
 
-data "template_file" "lauch" {
+data "template_file" "launch" {
   template = <<EOF
 #!/bin/bash
 sudo apt-get update
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
 sudo usermod -aG docker $(whoami)
-sudo docker run -d --network host joaodore/node
+
+git clone https://github.com/joao-dore/challenge-ia.git
+sudo mv challenge-ia/ /home/ubuntu/
+cd /home/ubuntu/challenge-ia/
+
+sudo apt install swarm
+sudo docker swarm init
+sudo docker stack deploy -c docker-compose.yml challenge
 
 EOF
 }
